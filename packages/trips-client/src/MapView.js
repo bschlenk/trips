51e8c0a3e95889cadd4e8169e3fd050037085e85
Brian Schlenker <bschlenk@umich.edu>
2018-01-14 13:16:32 -0800

    Update deps and add prop-types

--- packages/trips-client/src/MapView.js
+++ packages/trips-client/src/MapView.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { withGoogleMap, GoogleMap } from 'react-google-maps';
 import './MapView.css';
 
