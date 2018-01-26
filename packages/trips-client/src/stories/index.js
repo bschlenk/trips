13bc66b79a302e734676f4990b9f971fefc4fdc6
Brian Schlenker <bschlenk@umich.edu>
2018-01-25 23:27:02 -0800

    Add storybook

---
+++ packages/trips-client/src/stories/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button } from '@storybook/react/demo';
+import Estimate from '../pages/App/components/EstimateView/components/Estimate';
+
+storiesOf('Estimate', module)
+  .add('for Uber', () => (
+    <Estimate
+      service="Uber"
+      flavor="Uber X"
+      duration={200}
+      price={500}
+    />
+  ))
+  .add('for Lyft', () => (
+    <Estimate
+      service="Lyft"
+      flavor="Lyft Line"
+      duration={200}
+      price={500}
+    />
+  ))
+  .add('for car2go', () => (
+    <Estimate
+      service="car2go"
+      flavor="For Two"
+      duration={200}
+      price={500}
+    />
+  ))
+  .add('for ReachNow', () => (
+    <Estimate
+      service="ReachNow"
+      duration={200}
+      price={500}
+    />
+  ))
+  .add('with long duration', () => (
+    <Estimate
+      service="Uber"
+      duration={50000}
+      price={800}
+    />
+  ));
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
