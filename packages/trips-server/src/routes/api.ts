c71cdcce92010ee59badd41f10c5698076a64756
Brian Schlenker <bschlenk@umich.edu>
2018-01-25 23:09:27 -0800

    Properly parse coords from client

--- packages/trips-server/src/routes/api.ts
+++ packages/trips-server/src/routes/api.ts
@@ -1,7 +1,22 @@
 import * as Router from 'koa-router';
 import * as trips from '../api/trips';
+import { Location } from '../api/locations';
 
 const router = new Router();
+
+function parseCoords(value: string): Location {
+  const [lat, lng] = value.split(',')
+  if (!lat || !lng) {
+    throw new Error(
+      'Coordinates must be given in the form "<latitude>,<longitude>"');
+  }
+  const latitude = parseFloat(lat);
+  const longitude = parseFloat(lng);
+  if (isNaN(latitude) || isNaN(longitude)) {
+    throw new Error(`latitude or longitude is not a number: ${lat},${lng}`);
+  }
+  return { latitude, longitude };
+}
 
 router.get('/api/estimate/coords', async function(ctx, next) {
   const { start, end } = ctx.query;
@@ -11,7 +26,9 @@ router.get('/api/estimate/coords', async function(ctx, next) {
   if (!end) {
     ctx.throw('Query string parameter "end" is required', 400);
   }
-  ctx.body = await trips.getPriceEstimates(start, end);
+  const startCoords = parseCoords(start);
+  const endCoords = parseCoords(end);
+  ctx.body = await trips.getPriceEstimates(startCoords, endCoords);
 });
 
 router.get('/api/estimate/address', async function(ctx, next) {
