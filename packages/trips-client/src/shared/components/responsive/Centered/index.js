5565f7a7fcb1639d7de218fab78876a267e9ccb0
Brian Schlenker <bschlenk@umich.edu>
2018-02-18 21:50:55 -0800

    Fix centered to pass on class name

--- packages/trips-client/src/shared/components/responsive/Centered/index.js
+++ packages/trips-client/src/shared/components/responsive/Centered/index.js
@@ -2,11 +2,18 @@ import React, { Children }  from 'react';
 import cn from 'classnames';
 import './style.css';
 
-export default function(props) {
-  const only = Children.only(props.children);
+export default function({
+  className,
+  children,
+  ...props
+}) {
+  const only = Children.only(children);
   return React.cloneElement(
     only, {
-      className: cn('ResponsiveCentered',
-      only.props.className)
+      className: cn(
+        'ResponsiveCentered',
+        only.props.className,
+        className,
+      ),
     });
 }
