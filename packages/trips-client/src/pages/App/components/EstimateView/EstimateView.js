cd17c74ca64cbec165f9765a8ef98d2bcf25d0be
Brian Schlenker <bschlenk@umich.edu>
2018-03-15 00:56:51 -0700

    Switch EstimateView to functional component

--- packages/trips-client/src/pages/App/components/EstimateView/EstimateView.js
+++ packages/trips-client/src/pages/App/components/EstimateView/EstimateView.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import range from 'lodash/range';
 
@@ -8,24 +8,25 @@ import Centered from 'components/Centered/Centered';
 
 import './EstimateView.css';
 
-export default class EstimateView extends Component {
-  static propTypes = {
-    estimates: PropTypes.arrayOf(PropTypes.object).isRequired,
-  }
-
-  render() {
-    const { loading } = this.props;
-    return (
-      <Centered>
-        <ul className="EstimateView">
-          {
-            loading
-              ? range(4).map(() => <EstimateFacade />)
-              : this.props.estimates.map(estimate => (
-                <EstimateWrapper {...{ estimate }} />))
-          }
-        </ul>
-      </Centered>
-    );
-  }
+export default function EstimateView({
+  estimates,
+  loading,
+}) {
+  return (
+    <Centered>
+      <ul className="EstimateView">
+        {
+          loading
+            ? range(4).map(() => <EstimateFacade />)
+            : this.props.estimates.map(estimate => (
+              <EstimateWrapper {...{ estimate }} />))
+        }
+      </ul>
+    </Centered>
+  );
 }
+
+EstimateView.propTypes = {
+  estimates: PropTypes.arrayOf(PropTypes.object).isRequired,
+  loading: PropTypes.bool,
+};
