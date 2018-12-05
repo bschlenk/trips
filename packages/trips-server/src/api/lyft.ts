a648ac29a86d2382e38552be3a9e71f9f56ebe8f
Brian Schlenker <bschlenk@umich.edu>
2018-12-04 19:31:54 -0500

    refactor: switch to lyft-client
    Switched to lyft-client which exports its own TypeScript definitions.

--- packages/trips-server/src/api/lyft.ts
+++ packages/trips-server/src/api/lyft.ts
@@ -1,31 +1,21 @@
 import * as _debug from 'debug';
-import * as Lyft from 'lyft-node';
+import Lyft, { RideType } from 'lyft-client';
 import * as querystring from 'querystring';
-import { Estimate, EstimateProvider, EstimateResult } from './estimate';
+import { EstimateProvider, EstimateResult } from './estimate';
 import { Location } from './locations';
 import Service from './service';
 
 const debug = _debug('app:lyft');
 
-const {
-  LYFT_CLIENT_ID,
-  LYFT_CLIENT_SECRET,
-} = process.env;
+const { LYFT_CLIENT_ID, LYFT_CLIENT_SECRET } = process.env;
 
-const lyft = new Lyft(LYFT_CLIENT_ID!, LYFT_CLIENT_SECRET!);
-
-const rideTypes: Lyft.RideType[] = [
-  'lyft',
-  'lyft_line',
-  'lyft_plus',
-];
+const LYFT = new Lyft(LYFT_CLIENT_ID!, LYFT_CLIENT_SECRET!);
 
 /**
  * @see https://developer.lyft.com/docs/universal-links
  */
-function createDeepLink(start: Location, end: Location, flavor: Lyft.RideType) {
+function createDeepLink(start: Location, end: Location, flavor: RideType) {
   const qs = querystring.stringify({
-    // TODO: can this not be lyft_line?
     id: flavor,
     partner: LYFT_CLIENT_ID,
     'pickup[latitude]': start.latitude,
@@ -38,40 +28,40 @@ function createDeepLink(start: Location, end: Location, flavor: Lyft.RideType) {
 }
 
 const provider: EstimateProvider = {
-  getPriceEstimates(start: Location, end: Location): Promise<EstimateResult[]> {
-    return Promise.all(rideTypes.map(async (rideType) => {
-      try {
-        const response = await lyft.getRideEstimates({ start, end, rideType });
-        const data = response.cost_estimates[0];
-        if (data.error_message) {
-          return {
-            service: Service.LYFT,
-            error: data.error_message,
-          };
-        }
+  async getPriceEstimates(
+    start: Location,
+    end: Location,
+  ): Promise<EstimateResult[]> {
+    try {
+      const response = await LYFT.getRideEstimates({ start, end });
+      debug('returning estimates for %d lyft services', response.length);
 
-        debug('returning data from lyft');
-        const link = createDeepLink(start, end, rideType);
+      return response.map(ride => {
+        const link = createDeepLink(start, end, ride.rideType);
         return {
           service: Service.LYFT,
           estimate: {
             link,
-            flavor: data.display_name,
-            duration: data.estimated_duration_seconds,
+            flavor: ride.displayName,
+            duration: ride.estimatedDurationSeconds,
             price: {
-              high: data.estimated_cost_cents_max,
-              low: data.estimated_cost_cents_min,
+              high: ride.estimatedCostCentsMax,
+              low: ride.estimatedCostCentsMin,
             },
           },
         };
-      } catch (err) {
-        debug('returning error from lyft: %j', err);
-        return {
+      });
+    } catch (err) {
+      debug('returning error from lyft: %j', err);
+      return [
+        {
           service: Service.LYFT,
-          error: `There was an error getting an estimate from Lyft: ${err.message}`,
-        };
-      }
-    }));
+          error: `There was an error getting an estimate from Lyft: ${
+            err.message
+          }`,
+        },
+      ];
+    }
   },
 };
 
